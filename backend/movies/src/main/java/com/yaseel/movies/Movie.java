cb99e21ab0aa633ff85474b0b0f166945b17d712
Aseel <aseelmahmood12@gmail.com>
2025-04-08 00:21:03 +0200

    Initial commit

---
+++ backend/movies/src/main/java/com/yaseel/movies/Movie.java
@@ -0,0 +1,62 @@
+package com.yaseel.movies;
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getTrailerLink() {
+        return trailerLink;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public List<String> getBackdrops() {
+        return backdrops;
+    }
+
+    public List<Review> getReviewIds() {
+        return reviewIds;
+    }
+}
---
+++ backend/movies/src/main/java/com/yaseel/movies/Review.java
@@ -0,0 +1,37 @@
+package com.yaseel.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
